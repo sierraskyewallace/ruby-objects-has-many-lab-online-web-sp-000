a143366242963f2b7ea1bf571edd9d73f6b52fe8
Sierra Wallace <sskwallace@gmail.com>
2020-09-24 02:13:29 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,9 @@
 class Author 
-  attr_accessor :name, :posts
+  attr_accessor :name 
   @@post_count = 0 
+  
   def initialize(name)
     @name = name
-    @posts = [] 
   end
-  def posts 
-    Posts.all.select {|post| post.author == self}
-  end
-end
+  
+  def 
