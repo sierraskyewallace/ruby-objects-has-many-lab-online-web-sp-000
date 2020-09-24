cc43e50944f55f618e6285ea8f3b232e70d540c5
Sierra Wallace <sskwallace@gmail.com>
2020-09-24 01:34:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,15 @@
-require 'pry'
 class Artist
   attr_accessor :name
  
   def initialize(name)
     @name = name
-    @songs = []
   end
  
   def add_song(song)
-    @songs << song
     song.artist = self
   end
  
   def songs
-    @songs
+    Song.all.select {|song| song.artist == self}
   end
 end
