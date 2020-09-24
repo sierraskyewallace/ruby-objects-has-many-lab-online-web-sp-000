8e368b3974889a3077cb2489e643efe14717fa7d
Sierra Wallace <sskwallace@gmail.com>
2020-09-24 02:29:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,13 +6,13 @@ class Author
     @name = name
   end
   def add_post(post)
-    binding.pry
     post.author == self 
   end
   def posts 
     Post.all.select {|post| post.author = self}
   end
   def add_post_by_title(title)
+    binding.pry
     post = Post.new(title)
     post.author = self
     self.add_post(title)
