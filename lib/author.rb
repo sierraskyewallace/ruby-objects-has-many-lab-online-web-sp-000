6f14a340ea85132d77e133726aa05db7567ae906
Sierra Wallace <sskwallace@gmail.com>
2020-09-24 02:09:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -4,11 +4,11 @@ class Author
   def initialize(name)
     @name = name
   end
-  def posts 
-    Post.all.select {|post| post.author == self}
-  end
   def add_post(post)
     post.author = self
+  end
+   def posts 
+    Post.all.select {|post| post.author == self}
   end
 end
 
