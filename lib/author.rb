92038f610b5c74bdd3e9bd69445a7dc38502bcf9
Sierra Wallace <sskwallace@gmail.com>
2020-09-24 02:28:22 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,20 +1,17 @@
+require 'pry'
 class Author 
   attr_accessor :name
   @@post_count = 0 
-  
   def initialize(name)
     @name = name
   end
-  
   def add_post(post)
-    binding.pry
+    #binding.pry
     post.author == self 
   end
-  
   def posts 
     Post.all.select {|post| post.author = self}
-end
-
+  end
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
