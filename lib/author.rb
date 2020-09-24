02098d17507a6ca4b1a6409e1c12d502065adb0f
Sierra Wallace <sskwallace@gmail.com>
2020-09-24 02:31:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,7 +9,7 @@ class Author
     post.author == self 
   end
   def posts 
-    Post.all.select {|post| post.author = self}
+    Post.all.select {|post| post.author == self}
   end
   def add_post_by_title(title)
     #binding.pry
